b5b0327fced27833664473ebdb5c33cc5f49803b
Eduardo Dos Santos Pio <124411117+eduardodspio@users.noreply.github.com>
2023-07-12 10:08:06 -0400

    Update demographic_data_analyzer.py

--- Demographic Data Analyzer/demographic_data_analyzer.py
+++ Demographic Data Analyzer/demographic_data_analyzer.py
@@ -16,7 +16,7 @@ def calculate_demographic_data(print_data=True):
 
     percentage_bachelors = round(non_percentage_bachelors / len(df) * 100, 1)
 
-    # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
+    # What percentage of people with advanced education ('Bachelors', 'Masters', or 'Doctorate') make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
